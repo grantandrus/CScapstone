fa3d082991f5a9bf8bb18eeed268d91d54c38ec1
annajacobs1 <annajacobs1@mail.weber.edu>
2026-05-19 16:00:06 -0600

    Add college tab (set up for when we have DB access but not functional yet)

---
+++ CS4760GrantApplication/Controllers/CollegeController.cs
@@ -0,0 +1,56 @@
+﻿using CS4760GrantApplication.Data;
+using CS4760GrantApplication.Models;
+using CS4760GrantApplication.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace CS4760GrantApplication.Controllers
+{
+    public class CollegeController : Controller
+    {
+        private readonly CS4760GrantApplicationContext _context;
+
+        public CollegeController(CS4760GrantApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: College/Add
+        public IActionResult Add()
+        {
+            // Without the database set up, these lines are causing an error. Will add back in when we have our DB
+            //var users = _context.Users.Select(u => new { u.Id, FullName = u.FirstName + " " + u.LastName }).ToList();
+            //ViewData["Deans"] = new SelectList(users, "Id", "FullName");
+            return View();
+        }
+
+        // POST: College/Add
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add(CollegeAddViewModel newCollege)
+        {
+            College college = new College();
+
+            if (ModelState.IsValid)
+            {
+                // if a dean was selected, find that user and assign them to the college
+                if (newCollege.DeanId.HasValue)
+                {
+                    college.Dean = await _context.Users.FindAsync(newCollege.DeanId.Value);
+                }
+                college.Name = newCollege.Name;
+
+                _context.Add(college);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+
+            // again, add these lines back in when DB is set up
+            //var users = _context.Users.Select(u => new { u.Id, FullName = u.FirstName + " " + u.LastName }).ToList();
+            //ViewData["Deans"] = new SelectList(users, "Id", "FullName", newCollege.DeanId);
+            return View(college);
+        }
+    }
+}
---
+++ CS4760GrantApplication/ViewModels/CollegeAddViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CS4760GrantApplication.ViewModels
+{
+    public class CollegeAddViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+        public int? DeanId { get; set; }
+    }
+}
