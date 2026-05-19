9d14eab2e5b46ffbadf867251d658d08dc89cc20
TyFleisch10 <tylerfleischel@hotmail.com>
2026-05-19 13:17:32 -0600

    Add department functionality

---
+++ CS4760GrantApplication/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CS4760GrantApplication.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string DepartmentName { get; set; } = string.Empty;
+
+        [StringLength(500)]
+        public string? Description {  get; set; }
+    }
+}
