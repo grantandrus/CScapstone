46e1c870c381a2d913b372fb63ba73fdbcf90789
grantandrus <grantandrus2004@gmail.com>
2026-05-20 13:43:46 -0600

    feat(grant_request): added grant application form and small dashboard settup

---
+++ CS4760GrantApplication/Controllers/GrantController.cs
@@ -0,0 +1,39 @@
+﻿using CS4760GrantApplication.Data;
+using CS4760GrantApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CS4760GrantApplication.Controllers
+{
+    public class GrantController : Controller
+    {
+        private readonly CS4760GrantApplicationContext _context;
+
+        public GrantController(CS4760GrantApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        public async Task<IActionResult> Create(Grant grant)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: once database conneciton is up, add user id as foriegn key
+                _context.Add(grant);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Dashboard");
+            }
+
+            return View(grant);
+        }
+    }
+}
---
+++ CS4760GrantApplication/Models/Grant.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CS4760GrantApplication.Models
+{
+    public class Grant
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string title { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string description { get; set; }
+        [StringLength(400)]
+        public int? UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
