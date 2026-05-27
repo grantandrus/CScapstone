235e255aa177e3e0f0a0bb053ff5547e0e920262
grantandrus <grantandrus2004@gmail.com>
2026-05-27 14:45:07 -0600

    feat(grant_updates): Skeleton for dashboard and guts for grant application

--- CS4760GrantApplication/Models/Grant.cs
+++ CS4760GrantApplication/Models/Grant.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
 
 namespace CS4760GrantApplication.Models
 {
@@ -8,13 +9,31 @@ namespace CS4760GrantApplication.Models
 
         [Required]
         [StringLength(50)]
-        public string title { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
 
         [Required]
         [StringLength(300)]
-        public string description { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
 
-        public int? UserId { get; set; }
+        [Required]
+        [StringLength(5000)]
+        public string ProjectSummary { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(5000)]
+        public string Justification { get; set; } = string.Empty;
+
+        [Required]
+        public int ProjectImpact { get; set; }
+
+        public bool isMultipleDepartments { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public Department? Department { get; set; }
+
+        public int UserId { get; set; }
+
         public User? User { get; set; }
 
         public bool InvolvesHumanOrAnimalSubjects { get; set; }
