99771b5e4f781ac5819d9b04df314ff03236d5f2
annajacobs1 <annajacobs1@mail.weber.edu>
2026-05-27 15:42:10 -0600

    ARCC tab groundwork

---
+++ CS4760GrantApplication/Controllers/ARCCController.cs
@@ -0,0 +1,50 @@
+﻿using CS4760GrantApplication.Data;
+using CS4760GrantApplication.Models;
+using CS4760GrantApplication.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CS4760GrantApplication.Controllers
+{
+    public class ARCCController : Controller
+    {
+        private readonly CS4760GrantApplicationContext _context;
+        public ARCCController(CS4760GrantApplicationContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var model = new ARCCViewModel
+            {
+                Members = _context.Users.Where(u => u.IsCommitteeMember).ToList(),
+                OtherUsers = _context.Users.Where(u => !u.IsCommitteeMember).ToList()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Remove(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user != null)
+            {
+                user.IsCommitteeMember = false;
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Add(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user != null)
+            {
+                user.IsCommitteeMember = true;
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CS4760GrantApplication/ViewModels/ARCCViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CS4760GrantApplication.Models;
+
+namespace CS4760GrantApplication.ViewModels
+{
+    public class ARCCViewModel
+    {
+        public List<User> Members { get; set; }
+        public List<User> OtherUsers { get; set; }
+    }
+}
