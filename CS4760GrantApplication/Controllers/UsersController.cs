7d6fe85b93b4d43c2b6f3f2d1f34cb8a2ca20638
MayKB <54408739+MayKB@users.noreply.github.com>
2026-06-02 23:06:01 -0600

    Added user profile page and ability to edit info and password

--- CS4760GrantApplication/Controllers/UsersController.cs
+++ CS4760GrantApplication/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using CS4760GrantApplication.Filters;
 using CS4760GrantApplication.Models;
 using CS4760GrantApplication.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -18,9 +19,23 @@ namespace CS4760GrantApplication.Controllers
             _context = context;
         }
 
+        [SessionAuthorize]
         public async Task<IActionResult> Index()
         {
-            return View();
+            var userId = HttpContext.Session.GetInt32("UserID");
+
+            if (userId == null)
+                return RedirectToAction("Login", "Users");
+
+            var user = await _context.Users
+                .Include(u => u.College)
+                .Include(u => u.Department)
+                .FirstOrDefaultAsync(u => u.Id == userId);
+
+            if (user == null)
+                return NotFound();
+
+            return View(user);
         }
 
         [HttpGet]
@@ -71,7 +86,7 @@ namespace CS4760GrantApplication.Controllers
             var colleges = _context.Colleges.ToList();
             var departments = _context.Departments.ToList();
 
-            colleges.Insert(0, new Models.College() { Id = 0, Name = "None" });
+            colleges.Insert(0, new College() { Id = 0, Name = "None" });
             departments.Insert(0, new Department() { Id = 0, DepartmentName = "None" });
 
             ViewBag.CollegeList = new SelectList(colleges, "Id", "Name");
@@ -123,6 +138,138 @@ namespace CS4760GrantApplication.Controllers
 
             return View(regUser);
         }
-        
+
+        [SessionAuthorize]
+        public async Task<IActionResult> Edit()
+        {
+            var userId = HttpContext.Session.GetInt32("UserID");
+
+            if (userId == null)
+                return RedirectToAction("Login", "Users");
+
+            var user = await _context.Users.FindAsync(userId);
+
+            if (user == null)
+                return RedirectToAction("Login", "Users");
+
+            var viewModel = new EditProfileViewModel
+            {
+                UserID = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                CollegeId = user.CollegeId,
+                DepartmentId = user.DepartmentId
+            };
+
+            var colleges = _context.Colleges.ToList();
+            var departments = _context.Departments.ToList();
+
+            colleges.Insert(0, new College() { Id = 0, Name = "None" });
+            departments.Insert(0, new Department() { Id = 0, DepartmentName = "None" });
+
+            ViewBag.CollegeList = new SelectList(colleges, "Id", "Name", user.CollegeId);
+            ViewBag.DepartmentList = new SelectList(departments, "Id", "DepartmentName", user.DepartmentId);
+
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [SessionAuthorize]
+        public async Task<IActionResult> Edit(EditProfileViewModel user)
+        {
+            var id = HttpContext.Session.GetInt32("UserID");
+            var emailCheck = await _context.Users.FirstOrDefaultAsync(u => u.Email == user.Email);
+
+            if (id != user.UserID)
+                return RedirectToAction("Login", "Users");
+
+            var existing = await _context.Users.FindAsync(id);
+
+            if (existing == null)
+                return RedirectToAction("Login", "Users");
+
+            existing.FirstName = user.FirstName;
+            existing.LastName = user.LastName;
+
+            if (emailCheck == null) // If not in use
+            {
+                existing.Email = user.Email;
+            }
+            else if (existing.Email != user.Email && emailCheck != null) // If not user's current email but is in use by another
+            {
+                var colleges = _context.Colleges.ToList();
+                var departments = _context.Departments.ToList();
+                colleges.Insert(0, new College() { Id = 0, Name = "None" });
+                departments.Insert(0, new Department() { Id = 0, DepartmentName = "None" });
+                ViewBag.CollegeList = new SelectList(colleges, "Id", "Name", user.CollegeId);
+                ViewBag.DepartmentList = new SelectList(departments, "Id", "DepartmentName", user.DepartmentId);
+
+                ViewBag.Error = "An account with that email already exists.";
+                return View();
+            }
+
+            if (user.CollegeId != null && user.CollegeId != 0)
+            {
+                existing.CollegeId = user.CollegeId;
+            }
+
+            if (user.DepartmentId != null && user.DepartmentId != 0)
+            {
+                existing.DepartmentId = user.DepartmentId;
+            }
+
+            await _context.SaveChangesAsync();
+
+            HttpContext.Session.SetString("Name", $"{user.FirstName} {user.LastName}");
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [SessionAuthorize]
+        public async Task<IActionResult> EditPassword()
+        {
+            var userId = HttpContext.Session.GetInt32("UserID");
+
+            if (userId == null)
+                return RedirectToAction("Login", "Users");
+
+            var user = await _context.Users.FindAsync(userId);
+
+            if (user == null)
+                return RedirectToAction("Login", "Users");
+
+            var viewModel = new EditPasswordViewModel
+            {
+                UserID = user.Id,
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [SessionAuthorize]
+        public async Task<IActionResult> EditPassword(EditPasswordViewModel user)
+        {
+            var id = HttpContext.Session.GetInt32("UserID");
+            var hasher = new PasswordHasher<User>();
+
+            if (id != user.UserID)
+                return RedirectToAction("Login", "Users");
+
+            var existing = await _context.Users.FindAsync(id);
+
+            if (existing == null)
+                return RedirectToAction("Login", "Users");
+
+            existing.PasswordHash = hasher.HashPassword(existing, user.Password);
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
---
+++ CS4760GrantApplication/ViewModels/EditProfileViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CS4760GrantApplication.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CS4760GrantApplication.ViewModels
+{
+    public class EditProfileViewModel
+    {
+        public int UserID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public int? CollegeId { get; set; }
+        public int? DepartmentId { get; set; }
+    }
+}
