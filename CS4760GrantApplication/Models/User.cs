30390ab918460598f2405768e4d6238272466ec2
MayKB <54408739+MayKB@users.noreply.github.com>
2026-06-03 13:35:07 -0600

    fix: User IsCommitteeChair and Rubric Style fixes

--- CS4760GrantApplication/Models/User.cs
+++ CS4760GrantApplication/Models/User.cs
@@ -21,6 +21,7 @@ namespace CS4760GrantApplication.Models
         public bool IsAdmin { get; set; } = false;
         public List<Grant> Grants { get; set; } = new();
         public bool IsCommitteeMember { get; set; } = false;
+        public bool IsCommitteeChair { get; set; } = false;
         public int? CollegeId { get; set; }
         public College? College { get; set; }
         public int? DepartmentId { get; set; }
