12fbaaa3c4c465fe9dcb45702ca7149add10adb8
TyFleisch10 <tylerfleischel@hotmail.com>
2026-06-10 16:52:32 -0600

    Add committee chair allocations tab

---
+++ CS4760GrantApplication/Models/Allocation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CS4760GrantApplication.Models
+{
+    public class Allocation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Precision(18, 2)]
+        public decimal AvailableAmount { get; set; }
+
+        [Required]
+        [Precision(18, 2)]
+        public decimal RolloverAmount { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public decimal CutoffPercent { get; set; }
+    }
+}
