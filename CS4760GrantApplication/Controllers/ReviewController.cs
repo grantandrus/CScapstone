283a1194ca04c7902c8f0b2895956fedca87f1b0
grantandrus <grantandrus2004@gmail.com>
2026-06-10 14:57:34 -0600

    feat(review_tab): Created admin review tab to view all submited grants and how much money arcc is set to donate

---
+++ CS4760GrantApplication/Controllers/ReviewController.cs
@@ -0,0 +1,35 @@
+﻿using CS4760GrantApplication.Data;
+using CS4760GrantApplication.Filters;
+using CS4760GrantApplication.Models;
+using CS4760GrantApplication.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace CS4760GrantApplication.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private readonly CS4760GrantApplicationContext _context;
+
+        public ReviewController(CS4760GrantApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public List<Grant> Grants = new();
+
+        [SessionAuthorize]
+        public async Task<IActionResult> Index()
+        {
+
+            Grants = await _context.Grants
+                .Include(g => g.BudgetItems)
+                .Include(g => g.User)
+                .Where(g => !g.IsSaved)
+                .ToListAsync();
+
+            return View(Grants);
+        }
+    }
+}
