ae326083ad9d5c3fb78826550578b1dd775dc75e
SyPhase <pi2568gamerz@gmail.com>
2026-06-10 05:59:57 -0600

    Updated to show submitted (saved) grant applications on ARCC dashboard
    - ARCCViewModel.cs: added property to hold submitted grants: List<Grant> SubmittedGrants.
    - ARCCController.cs: added SubmittedGrants to IActionResult Index() call (under model).
    - Updated view Index.cshtml: added table to show submitted grants (title, priciple investigator, funds requested, and actions)

--- CS4760GrantApplication/Models/Grant.cs
+++ CS4760GrantApplication/Models/Grant.cs
@@ -43,7 +43,7 @@ namespace CS4760GrantApplication.Models
         public User? User { get; set; }
 
         public bool InvolvesHumanOrAnimalSubjects { get; set; }
-        public bool IsSaved { get; set; }
+        public bool IsSaved { get; set; } // When true, the grant is submitted and ready for review. When false, it's still a draft.
         public List<GrantAttachment> Attachments { get; set; } = new();
         public List<BudgetItem> BudgetItems { get; set; } = new();
     }
--- CS4760GrantApplication/ViewModels/ARCCViewModel.cs
+++ CS4760GrantApplication/ViewModels/ARCCViewModel.cs
@@ -4,8 +4,9 @@ namespace CS4760GrantApplication.ViewModels
 {
     public class ARCCViewModel
     {
-        public List<User> Members { get; set; }
-        public List<User> OtherUsers { get; set; }
+        public List<User> Members { get; set; } = new();
+        public List<User> OtherUsers { get; set; } = new();
         public int? CurrentChairId { get; set; }
+        public List<Grant> SubmittedGrants { get; set; } = new(); // Final grants submitted for review
     }
 }
