f857ac23696bb0976ef1110563e0cd4cf67e582e
MayKB <54408739+MayKB@users.noreply.github.com>
2026-06-29 19:31:58 -0600

    feat: export grants as xcel file

--- CS4760GrantApplication/Controllers/AllocationsController.cs
+++ CS4760GrantApplication/Controllers/AllocationsController.cs
@@ -1,18 +1,24 @@
-﻿using CS4760GrantApplication.Data;
+﻿using ClosedXML.Excel;
+using CS4760GrantApplication.Data;
 using CS4760GrantApplication.Filters;
 using CS4760GrantApplication.Models;
+using DocumentFormat.OpenXml.Spreadsheet;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace CS4760GrantApplication.Controllers
 {
     public class AllocationsController : Controller
     {
         private readonly CS4760GrantApplicationContext _context;
+        public IConfiguration Configuration { get; set; }
 
-        public AllocationsController(CS4760GrantApplicationContext context)
+        public AllocationsController(CS4760GrantApplicationContext context, IConfiguration _configuration)
         {
             _context = context;
+            Configuration = _configuration;
         }
 
         public List<Grant> Grants = new();
@@ -94,5 +100,43 @@ namespace CS4760GrantApplication.Controllers
             ViewBag.Message = "Allocations saved successfully.";
             return View(await _context.Allocations.FirstOrDefaultAsync());
         }
+
+        [SessionAuthorize]
+        [HttpPost]
+        public IActionResult Export()
+        {
+            using XLWorkbook wb = new();
+            DataTable dt = GetGrants().Tables[0];
+
+            var ws = wb.Worksheets.Add(dt);
+
+            ws.Columns("A").AdjustToContents();
+            ws.Columns("B").AdjustToContents().Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Left;
+            ws.Columns("C").AdjustToContents();
+
+            using MemoryStream stream = new();
+            wb.SaveAs(stream);
+            return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "GrantAllocations.xlsx");
+        }
+
+        private DataSet GetGrants()
+        {
+            DataSet ds = new();
+            var constr = Configuration.GetConnectionString("DefaultConnection");
+            string sql = "SELECT Grants.Title AS \"Grant Title\", " +
+                "Grants.UserId AS \"PI Account Number\", " +
+                "Users.FirstName + ' ' + Users.LastName AS \"PI Name\" " +
+                "FROM Grants " +
+                "JOIN Users ON Grants.UserId = Users.Id " +
+                "ORDER BY Grants.Id;";
+            using (SqlConnection con = new(constr))
+            {
+                using SqlDataAdapter sda = new(sql, con);
+                sda.Fill(ds);
+            }
+
+            return ds;
+        }
+
     }
 }
