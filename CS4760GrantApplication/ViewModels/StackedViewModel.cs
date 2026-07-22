0bf7e6a1880a43f00adae4fbbdb93261b9628ee9
MayKB <kelsiemayb@gmail.com>
2026-07-22 00:27:41 -0600

    initial ARCC report functionality

---
+++ CS4760GrantApplication/ViewModels/StackedViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CS4760GrantApplication.ViewModels
+{
+    public class StackedViewModel
+    {
+        required public string StackedDimensionOne { get; set; }
+        required public List<SimpleReportViewModel> LstData { get; set; }
+    }
+}
